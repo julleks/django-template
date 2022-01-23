3305ed618207e2c93d052015dfe91c21a4a89951
Alex Rozum <rozumalex@gmail.com>
2022-01-23 12:06:38 +0100

    ci: tag docker image with build id

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,9 @@ WORKDIR /app
 
 COPY app/ /app/
 
-RUN apt-get update \
-    && apt-get install -y git binutils libpq-dev libproj-dev gdal-bin libcurl4-openssl-dev libssl-dev python3-dev python-dev libffi-dev build-essential\
-    && apt-get clean
+RUN apt-get update && \
+    apt-get install -y git apt-utils binutils libpq-dev libproj-dev gdal-bin libcurl4-openssl-dev libssl-dev python3-dev python-dev libffi-dev build-essential && \
+    apt-get clean
 
 RUN mkdir -p /root/.ssh && \
     chmod 0700 /root/.ssh && \
@@ -16,11 +16,12 @@ ARG SSH_PRIVATE_KEY
 RUN echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa && \
     chmod 600 /root/.ssh/id_rsa
 
-RUN pip install poetry==1.1.12
+RUN /usr/local/bin/python -m pip install --upgrade pip &&\
+    pip install poetry==1.1.12
 
 COPY poetry.lock pyproject.toml /app/
-RUN poetry config virtualenvs.create false \
-    && poetry install --no-dev --no-interaction --no-ansi
+RUN poetry config virtualenvs.create false && \
+    poetry install --no-dev --no-interaction --no-ansi
 
 EXPOSE 80
 
--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,8 +6,11 @@ docker push $ECR_REGISTRY/$ECR_REPOSITORY:release-candidate
 CURRENT_MANIFEST=$(aws ecr batch-get-image --repository-name $ECR_REPOSITORY --image-ids imageTag=current-release --query 'images[].imageManifest' --output text)
 CANDIDATE_MANIFEST=$(aws ecr batch-get-image --repository-name $ECR_REPOSITORY --image-ids imageTag=release-candidate --query 'images[].imageManifest' --output text)
 
-[ $CURRENT_MANIFEST ] && aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag previous-release --image-manifest "$CURRENT_MANIFEST"
+if [ ! -z "$CURRENT_MANIFEST" ]
+then
+  aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag previous-release --image-manifest "$CURRENT_MANIFEST"
+fi
 
 aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag current-release --image-manifest "$CANDIDATE_MANIFEST"
 
-[ ! $CURRENT_MANIFEST ] && aws ecr batch-delete-image --repository-name $ECR_REPOSITORY --image-ids imageTag=release-candidate
+aws ecr batch-delete-image --repository-name $ECR_REPOSITORY --image-ids imageTag=release-candidate
