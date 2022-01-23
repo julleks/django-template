6db57c32778544511410136928f087b695cb85c7
Alex Rozum <rozumalex@gmail.com>
2022-01-23 10:46:02 +0100

    ci: update docker image tags

---
+++ deployment/deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:release-candidate .
+docker push $ECR_REGISTRY/$ECR_REPOSITORY:release-candidate
+
+CURRENT_MANIFEST=$(aws ecr batch-get-image --repository-name $ECR_REPOSITORY --image-ids imageTag=current-release --query 'images[].imageManifest' --output text)
+CANDIDATE_MANIFEST=$(aws ecr batch-get-image --repository-name $ECR_REPOSITORY --image-ids imageTag=release-candidate --query 'images[].imageManifest' --output text)
+
+[ $CURRENT_MANIFEST ] && aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag previous-release --image-manifest "$CURRENT_MANIFEST"
+
+aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag current-release --image-manifest "$CANDIDATE_MANIFEST"
+
+[ ! $CURRENT_MANIFEST ] && aws ecr batch-delete-image --repository-name $ECR_REPOSITORY --image-ids imageTag=release-candidate
